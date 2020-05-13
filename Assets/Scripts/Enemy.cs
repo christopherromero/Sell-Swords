fb87507718c8705eba7c0d7d2c93435ea2cd2caf
Christopher Romero <christopherromero11@gmail.com>
2020-05-12 23:02:23 -0700

    Added Enemy AI and moved controller scripts to dedicated folder

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Interactable
+{
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        // Attack enemy
+
+    }
+
+}
---
+++ Assets/Scripts/Stats/EnemyStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : CharacterStats
+{
+    public override void Die()
+    {
+        base.Die();
+
+        // Add ragdoll effect / death animation
+
+        // Add dropped loot
+
+        Destroy(gameObject);
+    }
+
+}
